a2201f9220e77f430797b19297b4f6bf45e9ef42
John <johncordwell3@icoud.com>
2026-01-06 00:18:14 -0600

    add malloc.c

---
+++ malloc.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* number of integers */
+    int n;
+
+    printf("How many integers would you like?: ");
+    scanf("%d", &n);
+    printf("\n");
+
+    int *arr = malloc(n * sizeof(n));
+
+    if (!arr)
+    {
+        printf("malloc failed");
+        return 1;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        arr[i] = i * 11;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        printf("i-th arr: %d\n", i);
+        printf("value at i-th arr: %d\n", arr[i]);
+        printf("address of i-th arr: %p\n", (void *)&arr[i]);
+        printf("\n");
+    }
+    return 0;
+}
